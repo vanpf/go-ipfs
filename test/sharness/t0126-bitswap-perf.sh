f07611c0aa601f93fa20c04801c317090f65765d
Erik Ingenito <erik@carbonfive.com>
2018-10-29 17:35:26 -0700

    Improve output

--- test/sharness/t0126-bitswap-perf.sh
+++ test/sharness/t0126-bitswap-perf.sh
@@ -5,7 +5,9 @@
 #
 
 test_description="Test multiple ipfs nodes transferring files via bitswap"
-timing_file="/tmp/`basename "$0"`-timings.txt"
+timing_file="`basename "$0"`-timings.json"
+node_count=4
+file_size_kb=10000
 
 . lib/test-lib.sh
 
@@ -23,10 +25,9 @@ timing_file="/tmp/`basename "$0"`-timings.txt"
 # Parameterize file size
 
 time_expect_success() {
-    echo -n "`basename "$0"`:$1: " >> $timing_file
     { time {
     test_expect_success "$1" "$2";
-    TIMEFORMAT=%5R:%5S:%5U;
+    TIMEFORMAT=%5R;
     }; } 2>> $timing_file
 }
 
@@ -35,32 +36,20 @@ check_file_fetch() {
   fhash=$2
   fname=$3
 
-   time_expect_success "node$node can fetch file" '
+  echo -n "node$1-receive-elapsed-sec: " >> $timing_file
+  time_expect_success "node$node can fetch file" '
     ipfsi $node cat $fhash > fetch_out$node
   '
 
   test_expect_success "file looks good" '
     test_cmp $fname fetch_out$node
   '
+
+  echo -n "node$1-receive-duplicate_blocks: " >> $timing_file &&
+  ipfsi $1 bitswap stat | sed -n -e 's/[[:space:]]dup blocks received: \([0-9].*\)$/\1/p' >> $timing_file
 }
 
-#  test_expect_success "node0 data transferred looks correct" '
-#    ipfsi 0 bitswap stat > stat0 &&
-#    grep "blocks sent: 126" stat0 > /dev/null &&
-#    grep "blocks received: 5" stat0 > /dev/null &&
-#    grep "data sent: 228113" stat0 > /dev/null &&
-#    grep "data received: 1000256" stat0 > /dev/null
-#  '
-#
-#  test_expect_success "node1 data transferred looks correct" '
-#    ipfsi 1 bitswap stat > stat1 &&
-#    grep "blocks received: 126" stat1 > /dev/null &&
-#    grep "blocks sent: 5" stat1 > /dev/null &&
-#    grep "data received: 228113" stat1 > /dev/null &&
-#    grep "data sent: 1000256" stat1 > /dev/null
-#  '
-
-node_count=7
+echo '' > $timing_file
 
 test_expect_success "set up tcp testbed" '
   iptb init -n $node_count -p 0 -f --bootstrap=none
@@ -76,9 +65,9 @@ do
   '
 done
 
-# Create a bit file
+# Create a big'ish file
 test_expect_success "add a file on node0" '
-  random 50000000 > filea &&
+  random $(($file_size_kb * 1024)) > filea &&
   FILEA_HASH=$(ipfsi 0 add -q filea)
 '
 
@@ -89,17 +78,11 @@ do
   sleep 2
 done
 
-for i in $(test_seq 0 $(expr $node_count - 1))
-do
-  echo "node $i:" >> $timing_file
-  ipfsi $i bitswap stat >> $timing_file
-done
-
 # shutdown
 test_expect_success "shut down nodes" '
   iptb stop && iptb_wait_stop
 '
 
-test_done
+cat $timing_file
 
-echo "Results saved in $timing_file"
+test_done
